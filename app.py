c2b7f09fa909d9ccca44aabcf7723538416ec4a8
michael <mdayan.comp@gmail.com>
2015-09-07 17:09:19 -0400

    Bokeh integration

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template, request, redirect
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from bokeh.embed import file_html
+
+plot = figure()
+plot.circle([1,2], [3,4])
+test_html = file_html(plot, CDN, "my plot")
 
 app = Flask(__name__)
 
@@ -9,6 +16,10 @@ def main():
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+@app.route('/stock_plot')
+def plot():
+  return test_html
 
 if __name__ == '__main__':
   app.run(port=33507)
